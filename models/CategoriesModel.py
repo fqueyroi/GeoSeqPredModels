b141c5bf23696068aa2a151fc341dfd1a3e7b15d
E175073X <Lc17101999>
2020-06-11 12:03:44 +0200

    Update on Categories Model

--- models/CategoriesModel.py
+++ models/CategoriesModel.py
@@ -4,33 +4,51 @@
 
 import FixOrderModel
 
-#TODO change to PredModel
 class CategoriesModel(FixOrderModel.FixOrderModel):
 
-    def __init__(self, maxContextLength, alphabet,  categories, seq, use_lprefix=True):
+    def __init__(self, maxContextLength, alphabet,  categories, use_lprefix=True):
         super(CategoriesModel, self).__init__(maxContextLength, alphabet, use_lprefix)
         self.categories = categories
-        self.seq = seq
+
 
     def __str__(self):
         return "Categories Model(" + str(self.maxContextLength) + ")"
 
     def learn(self, seq):
-        super(CategoriesModel, self).learn(seq)
+        """ Use to the sequence 'seq' to train the model
+        		Parameters:
+        		-----------
+        		seq: list of str
+        			The sequence of symbols to use
+        		"""
+        ## TODO this is a mess
+        if len(seq) == 0:
+            ## does nothing
+            return
+        if len(seq) == 1:
+            ## only add seq[0] to the base counter
+            self.tree.increment(seq[0], [])
+            return
+        if len(seq) <= self.maxContextLength:
+            self.tree.increment(seq[0], [])
+            for i in range(2, len(seq) + 1):
+                cseq = seq[:i]
+                self.tree.increment(cseq[-1], map(self.getCategory, cseq[:-1]))
+        else:
+            maxlength = self.maxContextLength
+            self.tree.increment(seq[0], [])
+            for i in range(2, maxlength + 1):
+                cseq = seq[:i]
+                self.tree.increment(cseq[-1], map(self.getCategory, cseq[:-1]))
+            for i in range(len(seq) - maxlength):
+                length = min(maxlength + 1, len(seq) - i)
+                cseq = seq[i:i + length]
+                self.tree.increment(cseq[-1], map(self.getCategory, cseq[:-1]))
 
     def getCategory(self,symbol):
-        for k, c in categories.iteritems():
-            if symbol in c:
-                return k
-
-    def seqToCategoriesAndSymbols(self, seq, symbol):
-        catSeq = []
-        for i in seq:
-            if i != symbol:
-                catSeq.append(self.getCategory(i))
-            else:
-                catSeq.append(i)
-        return catSeq
+        for k, c in self.categories.iteritems():
+            if k == symbol:
+                return c
 
     def seqToCategories(self, seq):
         catSeq = []
@@ -39,94 +57,37 @@ class CategoriesModel(FixOrderModel.FixOrderModel):
         return catSeq
 
     def probability(self, symbol, context):
-            #Model with categories and the given symbol
-            catModel = CategoriesModel(3, categories.keys(), categories, seq)
-            catSeq = self.seqToCategoriesAndSymbols(seq, symbol)
-            catModel.learn(catSeq)
-
-            #Model with only categories to count N(C(s))
-            totModel = CategoriesModel(3, categories.keys(), categories, seq)
-            totSeq = self.seqToCategories(seq)
-            totModel.learn(totSeq)
-
-            catContext = []
-            for i in context:
-                catContext.append(self.getCategory(i))
-
-            context_node_cat = totModel.tree.longestPrefix(catContext)
-            tot_count_n = context_node_cat.totalCount() + 0.
-            context_node = catModel.tree.getNode(catContext)
-
-            context_b = ''.join(context)
-            #additional research of pattern, context of len = 1
-            if len(context) == 1:
-                if symbol == context_b:
-                    res = catModel.tree.longestPrefix(context)
-                    return (context_node[symbol] + res[symbol]) / tot_count_n
-                elif self.getCategory(symbol) is self.getCategory(context[0]):
-                    context_cpt = symbol
-                    res = catModel.tree.longestPrefix(context_cpt)
-                    return (context_node[symbol] + res[symbol]) / tot_count_n
-            # context of len > 1
-            else:
-                if symbol in context:
-                    nvContext = []
-                    for i in context:
-                        if i != symbol:
-                            nvContext.append(self.getCategory(i))
-                        else:
-                            nvContext.append(i)
-                    node = catModel.tree.longestPrefix(nvContext)
-                    if context_node is not None and node[symbol] > context_node[symbol]:
-                        return (node[symbol] +  context_node[symbol]) / tot_count_n
-                    elif context_node is None and node[symbol] > context_node[symbol]:
-                        return (node[symbol]) / tot_count_n
-                #test solution
-                # elif self.getCategory(symbol) in catContext:
-                #     print "ici"
-                #     nvContext = []
-                #     print context
-                #     for i in context:
-                #         if self.getCategory(i) == self.getCategory(symbol):
-                #             nvContext.append(symbol)
-                #         else:
-                #             nvContext.append(self.getCategory(i))
-                #     print nvContext
-                #     res_test = catModel.tree.longestPrefix(nvContext)
-                #     return (res_test[symbol] + context_node[symbol]) / tot_count_n
-            if  context_node is None:
-                return 0
-            else:
-                return context_node[symbol] / tot_count_n
+        return super(CategoriesModel, self).probability(symbol,  map(self.getCategory, context))
 
 
 
-seq = 'aatccaagaatcg'
-alphabet = ['a', 'c', 'g', 't']
-categories = {'C1': ['a', 'c'], 'C2': ['g', 't']}
-
-model = CategoriesModel(3, alphabet, categories, seq)
-model.learn(seq)
-
-print "Tree : "
-print model.tree
-
-context = ['g', 'a']    #bug
-# context = ['a', 'a']   #bug should find 2/5 not 1/5 on a | a,a
-# context = ['a']
-
-for n in alphabet:
-    ncontext = context[:]
-    print n + " | " + ','.join(ncontext) + " : " + str(model.probability(n, ncontext))
+# seq = 'aatccaagaatcg'
+# alphabet = ['a', 'c', 'g', 't']
+# categories = {'a': 'C1', 'c': 'C1', 'g': 'C2', 't': 'C2'}
+#
+#
+# model = CategoriesModel(3, alphabet, categories)
+# model.learn(seq)
+#
+# print "Tree : "
+# print model.tree
+#
+# # context = ['g', 'a']
+# context = ['a', 'a']
+# # context = ['a']
+#
+# for n in alphabet:
+#     ncontext = context[:]
+#     print n + " | " + ','.join(ncontext) + " : " + str(model.probability(n, ncontext))
 
 # seq = 'abracadabra'
 # alphabet = set(seq)
 #
 # alphabet = ['a','b','r','c','d']
-# categories = {'C1': ['a','b', 'c'], 'C2': ['d', 'r']}
+# categories = {'a': 'C1', 'b': 'C1', 'c': 'C1', 'd': 'C2', 'r': 'C2'}
 #
 #
-# model = CategoriesModel(3, alphabet, categories, seq)
+# model = CategoriesModel(3, alphabet, categories)
 # model.learn(seq)
 #
 # print seq
@@ -135,9 +96,8 @@ for n in alphabet:
 # print "Tree : "
 # print model.tree
 #
-# context = ['d', 'a']
-# # context = ['r','a']
-# # context = ['a']
+# # context = ['d', 'a']
+# context = ['a']
 #
 #
 # for n in alphabet:
