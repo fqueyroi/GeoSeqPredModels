021fb4e89918f2d97f856c7ac67516b27868b94f
E175073X <Lc17101999>
2020-06-16 14:11:33 +0200

    Add CategoriesModel to GeoSeqsTestModels + add methods to filter sequences from the data set

--- data/LoadLlyodsData.py
+++ data/LoadLlyodsData.py
@@ -11,5 +11,62 @@ def getLocations():
 	locations = IOUtils.readLocations(locations_file_path,4,6,7)
 	return locations
 ################################################################################
-# print len(getSequences())
+def getCategories():
+	filename = os.path.dirname(__file__)+'/Lloyds_maritime/table_places.csv'
+	categories = dict()
+	file = open(filename,'r')
+	count_line = 0
+	header = True
+	col_id = 4
+	col_cat = 5			#5/Name_Loyds - 0/Continent - 3/Country
+	sep = ','
+	for line in file :
+		if header :
+			if count_line == 0:
+				count_line += 1
+				continue
+		if len(line) == 0:
+			count_line += 1
+			continue
+		count_line += 1
+		split_line = line.strip().split(sep)
+		if split_line[col_cat] == "" :
+			continue
+		id = split_line[col_id]
+		cat = split_line[col_cat]
+		categories[id] = cat
+
+	file.close()
+	return categories
+
+################################################################################
+
+def filterAlphabet(alphabet, categories):
+	filtered_alp = []
+	for i in alphabet:
+		if i in categories:
+			filtered_alp.append(i)
+	return filtered_alp
+################################################################################
+
+def filterSequences(sequences, categories):
+	filtered_seq = []
+	valid=True
+	for i in range(len(sequences)):
+		for j in range(len(sequences[i])):
+			if sequences[i][j] not in categories:
+				valid =False
+		if valid == True:
+			filtered_seq.append(sequences[i])
+	return filtered_seq
+
+
+
+# sequences = getSequences()
+# categories = getCategories()
+# print sequences
+# print categories
 # print getLocations()
+# print filterSequences(sequences, categories)
+
+
--- experiments/GeoSeqsTestModels.py
+++ experiments/GeoSeqsTestModels.py
@@ -14,6 +14,7 @@ import PPMCModel
 import FixOrderModel
 import ThereAndBackModel
 import GeoFixOrderModel
+import CategoriesModel
 import HONModel
 
 sys.path.append(''.join([os.path.dirname(__file__), '/..', '/data/']))
@@ -23,39 +24,41 @@ import LoadLlyodsData
 ### PARAMETERS
 ### (Should list all variables for the experiments)
 len_test = 3
-dataset = 'PortoTaxis'
-# dataset = 'Ports'
-min_k = 1 ## minimum context length
-max_k = 3 ## maximum context length
+# dataset = 'PortoTaxis'
+dataset = 'Ports'
+min_k = 1  ## minimum context length
+max_k = 3  ## maximum context length
 
 ## for Geo models
 ## TODO: Search procedure to automatically find the best gamma value ?
-gamma =  0.0000001
+gamma = 0.0000001
 dist_fun = GeoFixOrderModel.Dists[GeoFixOrderModel.DistCalc.HAVERSINE]
 
 ### Load Dataset
 sequences = []
 locations = []
 if dataset == 'PortoTaxis':
-	dist_fun = GeoFixOrderModel.Dists[GeoFixOrderModel.DistCalc.EUCLIDIAN]
-	sequences = LoadPortoTaxisData.getSequences(False, 0)
-	sequences = DataModUtils.removeRepetitions(sequences)
-	locations = LoadPortoTaxisData.getLocations()
+    dist_fun = GeoFixOrderModel.Dists[GeoFixOrderModel.DistCalc.EUCLIDIAN]
+    sequences = LoadPortoTaxisData.getSequences(False, 0)
+    sequences = DataModUtils.removeRepetitions(sequences)
+    locations = LoadPortoTaxisData.getLocations()
 if dataset == 'Ports':
-	sequences = LoadLlyodsData.getSequences()
-	sequences = DataModUtils.removeRepetitions(sequences)
-	locations = LoadLlyodsData.getLocations()
+    sequences = LoadLlyodsData.getSequences()
+    sequences = DataModUtils.removeRepetitions(sequences)
+    locations = LoadLlyodsData.getLocations()
+    categories = LoadLlyodsData.getCategories()
+    sequences = LoadLlyodsData.filterSequences(sequences, categories)
 
-print "Nb Seqs : "+str(len(sequences))
+print "Nb Seqs : " + str(len(sequences))
 
 ### Create Training/Testing subsets
 training, test_contexts, testing = [], [], []
 if dataset == 'PortoTaxis':
-	training, testing = DataModUtils.cutEndOfSequences(sequences, len_test)
-	test_contexts = training
+    training, testing = DataModUtils.cutEndOfSequences(sequences, len_test)
+    test_contexts = training
 if dataset == 'Ports':
-	training, testing = DataModUtils.cutEndOfSequences(sequences, len_test)
-	test_contexts = training
+    training, testing = DataModUtils.cutEndOfSequences(sequences, len_test)
+    test_contexts = training
 ## TODO: perform test not only on the last symbol in training sequence
 ## 		instead split all sequences into to set at random (training and testing)
 ##		and compute the prediction score by an average on the proba of
@@ -63,31 +66,36 @@ if dataset == 'Ports':
 
 ### Get the unique list of symbols found in sequences
 alphabet = SeqStats.symbols(sequences)
-print "Nb Symbols : "+str(len(alphabet))
+if dataset == 'Ports':
+    alphabet = LoadLlyodsData.filterAlphabet(alphabet, categories)
+
+print "Nb Symbols : " + str(len(alphabet))
 
 ### Filter locations (to match alphabet)
 loc_temp = dict()
 for a in alphabet:
-	if a in locations.keys():
-		loc_temp[a] = locations[a]
+    if a in locations.keys():
+        loc_temp[a] = locations[a]
 locations = loc_temp
-print "Nb Locations :"+str(len(locations.keys()))
+print "Nb Locations :" + str(len(locations.keys()))
+
 
 ### Set functions use to compare models
 def averageProbNextKSymbols(model, test_contexts, test_seqs, k):
-	## TODO: create file with evaluation functions including this one
-	## TODO: Test one using the distance between prediction and real location
-	res = [0 for i in range(k)]
-	order = model.maxContextLength
-	for i in range(len(test_seqs)):
-		t_seq = test_seqs[i][:min(k,len(test_seqs[i]))]
-		probs = model.probabilites(t_seq,test_contexts[i])
-		# print "Probs = "+str(probs)
-		p_temp = 1.
-		for j in range(len(t_seq)):
-			p_temp *= probs[j]
-			res[j] += p_temp / (len(test_seqs) + 0.)
-	return res
+    ## TODO: create file with evaluation functions including this one
+    ## TODO: Test one using the distance between prediction and real location
+    res = [0 for i in range(k)]
+    order = model.maxContextLength
+    for i in range(len(test_seqs)):
+        t_seq = test_seqs[i][:min(k, len(test_seqs[i]))]
+        probs = model.probabilites(t_seq, test_contexts[i])
+        # print "Probs = "+str(probs)
+        p_temp = 1.
+        for j in range(len(t_seq)):
+            p_temp *= probs[j]
+            res[j] += p_temp / (len(test_seqs) + 0.)
+    return res
+
 
 ## Init variables of GeoFixOrderModel
 max_d = GeoFixOrderModel.getMaxDistance(locations, dist_fun)
@@ -95,61 +103,72 @@ sum_d = GeoFixOrderModel.sumDensities(alphabet, locations, gamma, max_d, dist_fu
 
 ## TODO: output results in a file
 for i in range(min_k, max_k + 1):
-	ppmc = PPMCModel.PPMCModel(i, alphabet)
-	for seq in training:
-		ppmc.learn(seq)
+    ppmc = PPMCModel.PPMCModel(i, alphabet)
+    for seq in training:
+        ppmc.learn(seq)
 
-	tab = ThereAndBackModel.ThereAndBackModel(i, alphabet)
-	for seq in training:
-		tab.learn(seq)
+    tab = ThereAndBackModel.ThereAndBackModel(i, alphabet)
+    for seq in training:
+        tab.learn(seq)
 
-	hon = HONModel.HONModel(i, alphabet)
-	for seq in training:
-		hon.learn(seq)
-	hon.prune()
+    hon = HONModel.HONModel(i, alphabet)
+    for seq in training:
+        hon.learn(seq)
+    hon.prune()
 
-	fix = FixOrderModel.FixOrderModel(i, alphabet)
-	for seq in training:
-		fix.learn(seq)
+    fix = FixOrderModel.FixOrderModel(i, alphabet)
+    for seq in training:
+        fix.learn(seq)
 
-	geo = GeoFixOrderModel.GeoFixOrderModel(i, alphabet, locations, gamma,
-			dist_fun, max_d, sum_d,False)
-	for seq in training:
-		geo.learn(seq)
+    geo = GeoFixOrderModel.GeoFixOrderModel(i, alphabet, locations, gamma,
+                                            dist_fun, max_d, sum_d, False)
+    for seq in training:
+        geo.learn(seq)
 
-	geo_zp = GeoFixOrderModel.GeoFixOrderModel(i, alphabet, locations, gamma,
-			dist_fun, max_d, sum_d,True)
-	for seq in training:
-		geo_zp.learn(seq)
+    geo_zp = GeoFixOrderModel.GeoFixOrderModel(i, alphabet, locations, gamma,
+                                               dist_fun, max_d, sum_d, True)
+    for seq in training:
+        geo_zp.learn(seq)
 
-	probs_ppmc = averageProbNextKSymbols(ppmc, test_contexts, testing, len_test)
-	print str(ppmc)
-	print "	probs : "+ SeqStats.str_probs(probs_ppmc)
-	print "	size  : " + str(ppmc.size())
+    if dataset == 'Ports':
+        cat = CategoriesModel.CategoriesModel(i, alphabet, categories)
+        for seq in training:
+            cat.learn(seq)
 
-	probs_tab = averageProbNextKSymbols(tab, test_contexts, testing, len_test)
-	print str(tab)
-	print "	probs : "+ SeqStats.str_probs(probs_tab)
-	print "	size  : " + str(tab.size())
+    probs_ppmc = averageProbNextKSymbols(ppmc, test_contexts, testing, len_test)
+    print str(ppmc)
+    print "	probs : " + SeqStats.str_probs(probs_ppmc)
+    print "	size  : " + str(ppmc.size())
 
-	probs_fix = averageProbNextKSymbols(fix, test_contexts, testing, len_test)
-	print str(fix)
-	print "	probs : "+ SeqStats.str_probs(probs_fix)
-	print "	size  : " + str(fix.size())
+    probs_tab = averageProbNextKSymbols(tab, test_contexts, testing, len_test)
+    print str(tab)
+    print "	probs : " + SeqStats.str_probs(probs_tab)
+    print "	size  : " + str(tab.size())
 
-	probs_hon = averageProbNextKSymbols(hon, test_contexts, testing, len_test)
-	print str(hon)+" PRUNED"
-	print "	probs : "+ SeqStats.str_probs(probs_hon)
-	print "	size  : " + str(hon.size())
+    probs_fix = averageProbNextKSymbols(fix, test_contexts, testing, len_test)
+    print str(fix)
+    print "	probs : " + SeqStats.str_probs(probs_fix)
+    print "	size  : " + str(fix.size())
 
-	probs_geo = averageProbNextKSymbols(geo, test_contexts, testing, len_test)
-	print str(geo)
-	print "	probs : "+ SeqStats.str_probs(probs_geo)
-	print "	size  : " + str(geo.size())
+    probs_hon = averageProbNextKSymbols(hon, test_contexts, testing, len_test)
+    print str(hon) + " PRUNED"
+    print "	probs : " + SeqStats.str_probs(probs_hon)
+    print "	size  : " + str(hon.size())
 
-	probs_geo_zp = averageProbNextKSymbols(geo_zp, test_contexts, testing, len_test)
-	print str(geo_zp)+' ZERO PROB'
-	print "	probs : "+ SeqStats.str_probs(probs_geo_zp)
-	print "	size  : " + str(geo_zp.size())
+    probs_geo = averageProbNextKSymbols(geo, test_contexts, testing, len_test)
+    print str(geo)
+    print "	probs : " + SeqStats.str_probs(probs_geo)
+    print "	size  : " + str(geo.size())
 
-	print
+    probs_geo_zp = averageProbNextKSymbols(geo_zp, test_contexts, testing, len_test)
+    print str(geo_zp)+' ZERO PROB'
+    print "	probs : "+ SeqStats.str_probs(probs_geo_zp)
+    print "	size  : " + str(geo_zp.size())
+
+    if dataset == 'Ports':
+        probs_cat = averageProbNextKSymbols(cat, test_contexts, testing, len_test)
+        print str(cat)
+        print "	probs : " + SeqStats.str_probs(probs_cat)
+        print "	size  : " + str(cat.size())
+
+    print
