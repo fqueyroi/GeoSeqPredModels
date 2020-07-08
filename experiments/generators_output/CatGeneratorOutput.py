e0ed77b0c585c7e3261c005e067fbcd612802f04
Francois Queyroi <francois.queyroi@univ-nantes.fr>
2020-07-08 18:34:18 +0200

    Fix Hierar&Cat Generators Outputs

--- experiments/generators_output/CatGeneratorOutput.py
+++ experiments/generators_output/CatGeneratorOutput.py
@@ -33,67 +33,76 @@ def learning(func_table, base, context_lengths, training, testing):
     result = []
 
     for k, v in func_table.iteritems():
-        result_func = base.copy()
         ##Get informations and train each model
         func, args, name = func_table[k]
 
-        model = func(context_lengths, *args)
-        for seq in training:
-            model.learn(seq)
+        for context in context_lengths:
+            model = func(context, *args)
+            for seq in training:
+                model.learn(seq)
+            if name == "HON":
+                model.prune()
 
-        func1 = EvalFunctions.averageProbNextSymbol(model, testing)
-        func2 = EvalFunctions.averageProbAllSymbols(model, testing)
+            func1 = EvalFunctions.averageProbNextSymbol(model, testing)
+            func2 = EvalFunctions.averageProbAllSymbols(model, testing)
 
-        print str(model)
-        print "	probs averageProbNextSymbol: " + str(round(func1 * 100., 2))
-        print "	probs averageProbAllSymbols: " + str(round(func2 * 100., 2))
+            # print str(model)
+            # print "	probs averageProbNextSymbol: " + str(round(func1 * 100., 2))
+            # print "	probs averageProbAllSymbols: " + str(round(func2 * 100., 2))
 
-        result_func.update({'model': name, 'k': context_lengths, 'score_1': str(round(func1 * 100., 2)), 'score_2': str(round(func2 * 100., 2))})
-        result.append(result_func)
+            result_func = base.copy()
+            result_func.update({'model': name, 'context': context,
+                'size' : model.size(), 'score_1': str(round(func1 * 100., 2)),
+                'score_2': str(round(func2 * 100., 2))})
+            result.append(result_func)
     return result
 
 ### PARAMETERS
 ### (Should list all variables for the experiments)
 context_lengths = [1,2,3]
-a_size = [100, 1000, 10000]         #Alphabet size
-cat_size = [30,70,100]              #Numbers of categories
+a_size = [100, 700, 2000]         #Alphabet size
+cat_ratio = [0.01,0.1,0.3,0.6]              #Numbers of categories
 stop_prob = 0.1                     #Probability to stop a sequence
-alpha = 0.1                         #Transitions randomness
+alpha = [0.1]                         #Transitions randomness
 testing_ratio = 0.1                 #Sequence separation ratio
 repeat = 20
 
 
 result = []
 
-values = list(itertools.product(context_lengths, a_size, cat_size))
+values = list(itertools.product(a_size, cat_ratio,alpha))
 
 
 for i in values:
     for j in range(repeat):
         base = collections.OrderedDict()
         base['model'] = None
-        base['alphabet_size'] = i[1]
+        base['alphabet_size'] = i[0]
         base['stop_prob'] = stop_prob
-        base['k'] = None
-        base['categories_size'] = i[2]
-        base['alpha'] = alpha
+        base['context'] = None
+        base['cat_ratio'] = i[1]
+        base['alpha'] = i[2]
+        base['size']    = None
         base['score_1'] = None
         base['score_2'] = None
 
         ## Generate datasets
-        gen = CategoriesBasedGenerator.CategoriesBasedGenerator(alphabet_size=i[1], categories_size=i[2], stop_prob=stop_prob,
-                                                                alpha=alpha)
+        gen = CategoriesBasedGenerator.CategoriesBasedGenerator(alphabet_size=i[0],
+                categories_size=int(round(i[1]*i[0])), stop_prob=stop_prob,
+                alpha=i[2])
         categories = gen.locations
         locations = gen.locations.keys()
+        print "Nb Cats : " + str(int(round(i[1]*i[0])))
 
-        sequences = gen.generate(400)
+        sequences = gen.generate(1000)
         sequences = DataModUtils.removeRepetitions(sequences)
 
-        print "Nb Seqs : " + str(len(sequences))
+        # print "Nb Seqs : " + str(len(sequences))
 
         ### Get the unique list of symbols found in sequences
         alphabet = SeqStats.symbols(sequences)
         print "Nb Symbols : " + str(len(alphabet))
+        base['nb_symbols'] = len(alphabet)
 
 
         ### Create Training/Testing subsets
@@ -109,7 +118,7 @@ for i in values:
             5: (CategoriesModel.CategoriesModel, [alphabet, categories], "Categories"),
             6: (CategoriesAndSymbolModel.CategoriesAndSymbolModel, [alphabet, categories], "Categories and Symbol")
         }
-        result.append(learning(func_table, base, i[0], training, testing))
+        result.append(learning(func_table, base, context_lengths, training, testing))
 
 ##Write result in a file
 path_seq_file = sys.path[0] + '/RES_Categories_Generator.csv'
--- experiments/generators_output/HierarchicalReturnGeneratorOutput.py
+++ experiments/generators_output/HierarchicalReturnGeneratorOutput.py
@@ -31,70 +31,77 @@ def learning(func_table, base, context_lengths, training, testing):
     '''
     result = []
     for k, v in func_table.iteritems():
-        result_func = base.copy()
         ##Get informations and train each model
         func, args, name = func_table[k]
 
-        model = func(context_lengths, *args)
-        for seq in training:
-            model.learn(seq)
+        for context in context_lengths:
+            model = func(context, *args)
+            for seq in training:
+                model.learn(seq)
+            if name == "HON":
+                model.prune()
 
-        func1 = EvalFunctions.averageProbNextSymbol(model, testing)
-        func2 = EvalFunctions.averageProbAllSymbols(model, testing)
+            func1 = EvalFunctions.averageProbNextSymbol(model, testing)
+            func2 = EvalFunctions.averageProbAllSymbols(model, testing)
 
-        print str(model)
-        print "	probs averageProbNextSymbol: " + str(round(func1 * 100., 2))
-        print "	probs averageProbAllSymbols: " + str(round(func2 * 100., 2))
+            # print str(model)
+            # print "	probs averageProbNextSymbol: " + str(round(func1 * 100., 2))
+            # print "	probs averageProbAllSymbols: " + str(round(func2 * 100., 2))
 
-        result_func.update({'model': name, 'k': context_lengths, 'score_1': str(round(func1 * 100., 2)), 'score_2': str(round(func2 * 100., 2))})
-        result.append(result_func)
+            result_func = base.copy()
+            result_func.update({'model': name, 'context': context,
+                'size' : model.size(), 'score_1': str(round(func1 * 100., 2)),
+                'score_2': str(round(func2 * 100., 2))})
+            result.append(result_func)
     return result
 
 
 
 ### PARAMETERS
 ### (Should list all variables for the experiments)
-context_lengths = [1,2,3]
-a_size = [100, 1000, 10000]         #Alphabet size
-k_values = [3,5,7]                  #Average number of new connexion in the network
+context_lengths = [1,2]
+a_size = [100,800,2000]         #Alphabet size
+k_values = [3]                  #Average number of new connexion in the network
 stop_prob = 0.1                     #Probability to stop a sequence
-prob_return_node = 0.1              #Ratio of return node in the network
+prob_return_node = [0.01,0.1,0.3]             #Ratio of return node in the network
 testing_ratio = 0.1                 #Sequence separation ratio
 repeat = 20
 
 
 result = []
 
-values = list(itertools.product(context_lengths, a_size, k_values))
+values = list(itertools.product(a_size, k_values, prob_return_node))
 
 for i in values:
     for j in range(repeat):
         base = collections.OrderedDict()
         base['model'] = None
-        base['alphabet_size'] = i[1]
+        base['alphabet_size'] = i[0]
+        base['connect'] = i[1]
         base['stop_prob'] = stop_prob
-        base['prob_return_node'] = prob_return_node
-        base['k'] = None
+        base['prob_return_node'] = i[2]
+        base['context'] = None
+        base['size']    = None
         base['score_1'] = None
         base['score_2'] = None
 
         ## Generate datasets
-        gen = HierarchicalReturnGenerator.HierarchicalReturnGenerator(alphabet_size=i[1], k=i[2], stop_prob=stop_prob, prob_return_node=prob_return_node)
+        gen = HierarchicalReturnGenerator.HierarchicalReturnGenerator(alphabet_size=i[0],
+            k=i[1], stop_prob=stop_prob, prob_return_node=i[2])
         locations = gen.network
 
-        sequences = gen.generate(400)
+        sequences = gen.generate(1000)
         sequences = DataModUtils.removeRepetitions(sequences)
 
-        print "Nb Seqs : " + str(len(sequences))
+        # print "Nb Seqs : " + str(len(sequences))
 
         ### Get the unique list of symbols found in sequences
         alphabet = SeqStats.symbols(sequences)
-        print "Nb Symbols : " + str(len(alphabet))
-
+        # print "Nb Symbols : " + str(len(alphabet))
+        base['nb_symbols'] = len(alphabet)
         ### Create Training/Testing subsets
-        training, test_contexts, testing = [], [], []
+        training, testing = [], []
         training, testing = DataModUtils.sampleSequences(sequences, testing_ratio)
-        test_contexts = training
 
         func_table = {
             1: (PPMCModel.PPMCModel, [alphabet], "PPMC"),
@@ -102,7 +109,7 @@ for i in values:
             3: (HONModel.HONModel, [alphabet], "HON"),
             4: (FixOrderModel.FixOrderModel, [alphabet], "Fix Order"),
         }
-        result.append(learning(func_table, base, i[0], training, testing))
+        result.append(learning(func_table, base, context_lengths, training, testing))
 
 ##Write result in a file
 path_seq_file = sys.path[0] + '/RES_Return_Generator.csv'
@@ -112,7 +119,3 @@ with open(path_seq_file, 'w') as seq_file:
     for i in result:
         for j in i:
             csv_writer.writerow(j)
-
-
-
-
